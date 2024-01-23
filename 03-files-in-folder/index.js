f1ffe7662dd08087cd8fce0fbedbcc54e10edaf8
Konstantin Fokin <busbeet88@gmail.com>
2024-01-23 19:15:56 +0300

    fix: fix according to rule: information about files in subfolders should not be displayed   (Tue, Jan 23, 2024 07:15:56 PM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -14,21 +14,6 @@ fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
         const checkExtension = element.name.startsWith('.') ? element.name.slice(1) : path.extname(element.name).slice(1)
         stdout.write(`${element.name.slice(0, element.name.lastIndexOf("."))} - ${checkExtension} - ${stats.size}\n`)
       })
-    } else if (element.isDirectory()){
-      const pathToSubFolder = path.join(pathToFolder, element.name)
-      fs.readdir(pathToSubFolder, {withFileTypes: true}, (err, files) => {
-        if (err) return;
-        files.forEach((subElement) => {
-          if (subElement.isFile()) {
-            const pathToElement = path.join(pathToSubFolder, subElement.name)
-            fs.stat(pathToElement, (err, stats) => {
-              if (err) return;
-              const checkExtension = subElement.name.startsWith('.') ? subElement.name.slice(1) : path.extname(subElement.name).slice(1)
-              stdout.write(`${subElement.name.slice(0, subElement.name.lastIndexOf("."))} - ${checkExtension} - ${stats.size}\n`)
-            })
-          }
-        })
-      })
     }
   })
 })
