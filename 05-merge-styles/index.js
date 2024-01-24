2ad658bc3f31ddca843614345d7034500a9c586b
Konstantin Fokin <busbeet88@gmail.com>
2024-01-24 05:55:11 +0300

    fix: 05 and 06 merge function (Wed, Jan 24, 2024 05:55:11 AM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,21 +1,21 @@
 const fsPromises = require("fs/promises");
 const path = require("path");
 
-const bundlePath = path.join(__dirname + "/project-dist", "bundle.css")
+
 
 const getCssFiles = async (dir = __dirname, extension = ".css")  => {
+  const bundlePath = path.join(__dirname + "/project-dist", "bundle.css")
+  const stylesPath = path.join(__dirname, "styles")
+
   fsPromises.access(bundlePath, fsPromises.constants.F_OK)
     .then(() => fsPromises.unlink(bundlePath))
     .catch((error) => {})
-    .then(() => fsPromises.readdir(dir, {withFileTypes: true}))
+    .then(() => fsPromises.readdir(stylesPath, {withFileTypes: true}))
     .then((files) => {
       files.map((file) => {
-        const pathToFile = path.join(dir, file.name)
-          return fsPromises.stat(pathToFile)
+        const pathToFile = path.join(stylesPath, file.name)
+        return fsPromises.stat(pathToFile)
         .then((stats) => {
-          if(stats.isDirectory()) {
-            return getCssFiles(pathToFile, extension)
-          }
           if (stats.isFile() && path.extname(file.name) === extension) {
             fsPromises.readFile(pathToFile, {encoding: "utf8"})
               .then((content) => fsPromises.writeFile(bundlePath, content, {encoding: "utf8", flag: "a"}))
